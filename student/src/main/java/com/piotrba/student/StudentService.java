80c1aeac2373b792aaa19538922e220c75f5bcba
PiotrBa <piotr.balazyk@wp.pl>
2024-04-11 18:58:08 +0100

    Preparing the Entity, Service class and Repository for the add new student function.

--- student/src/main/java/com/piotrba/student/StudentService.java
+++ student/src/main/java/com/piotrba/student/StudentService.java
@@ -3,12 +3,13 @@ package com.piotrba.student;
 import org.springframework.stereotype.Service;
 
 @Service
-public record StudentService() {
+public record StudentService(StudentRepository studentRepository) {
     public void registerStudent(StudentRegistrationRequest request){
         Student student = Student.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
+        studentRepository.save(student);
     }
 }
