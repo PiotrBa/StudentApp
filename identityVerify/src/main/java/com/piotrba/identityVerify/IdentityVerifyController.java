0c0deff20ff2d842c000d371a4339f8a6cc8d2bd
PiotrBa <piotr.balazyk@wp.pl>
2024-05-15 10:45:40 +0100

    Code formatting (log.info: Fraud check request for student)

--- identityVerify/src/main/java/com/piotrba/identityVerify/IdentityVerifyController.java
+++ identityVerify/src/main/java/com/piotrba/identityVerify/IdentityVerifyController.java
@@ -1,6 +1,7 @@
 package com.piotrba.identityVerify;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/verify-check")
 @AllArgsConstructor
+@Slf4j
 public class IdentityVerifyController {
 
     private final IdentityVerifyService identityVerifyService;
@@ -16,6 +18,7 @@ public class IdentityVerifyController {
     public IdentityVerifyCheckResponse isFraudster
             (@PathVariable("userId") Long userId){
         boolean isfraudulentUser = identityVerifyService.isFraudulentUser(userId);
+        log.info("Fraud check request for student {}", userId);
         return new IdentityVerifyCheckResponse(isfraudulentUser);
     }
 }
