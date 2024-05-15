fd75b29eb272a27fafa357929b65f72d8f03057f
PiotrBa <piotr.balazyk@wp.pl>
2024-05-15 10:44:36 +0100

    Code formatting (imports)

--- student/src/main/java/com/piotrba/student/StudentController.java
+++ student/src/main/java/com/piotrba/student/StudentController.java
@@ -2,10 +2,7 @@ package com.piotrba.student;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/students")
